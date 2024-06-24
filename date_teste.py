0a0a30ca70b20bce13369ac9417bffb0ea9bc68f
TalitaBNalon <158466267+TalitaBNalon@users.noreply.github.com>
2024-06-24 07:55:28 -0300

    Create date_teste.py
    Criando um arquivo teste do date.py, para verificar se há erros no script.

---
+++ date_teste.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+# Obter a data e hora atual
+data_atual = datetime.now()
+
+# Convertendo para String
+data_convertida = data_atual.strftime('%Y-%m-%d')
+hora_atual = data_atual.strftime('%H:%M:%S')
+
+# Imprimindo a data e hora atuais
+print('Data atual: ', data_convertida)
+print('Hora atual: ', hora_atual)
+print('Data atual: ', data_convertida)
+print('Hora atual: ', hora_atual)
